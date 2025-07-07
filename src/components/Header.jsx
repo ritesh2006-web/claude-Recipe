253c816ed953376a558b3865ba3264d9df1b7ff2
ritesh2006-web <riteshpuri.231@gmail.com>
2025-07-07 12:21:50 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src="src\components\chef-claude-icon.png" alt="chef-logo" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./components/Apps";
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <App/>
+)
